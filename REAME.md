3948f9ee79330e66e027787ad78c59f53b256da8
benjaminhuanghuang <bhuang@rms.com>
2020-12-07 11:14:20 -0800

    update

--- REAME.md
+++ REAME.md
@@ -1,9 +1,20 @@
 # React + WebPack Project Boilerplate
 
+## Setup
+```
+    npm i webpack-cli webpack webpack-dev-server -D   
+
+    npm i react react-dom prop-types
+
+    npm i redux react-redux
+
+    npm i babel-loader @babel/core @babel/preset-env @babel/preset-react -D
+
+    npm i css-loader style-loader sass-loader node-sass -D
+```
+
 ## Key points
-- Babel
-    - ES6,7
-    - React
+- ES6, React
 
 - CSS / SCSS
 
@@ -15,19 +26,25 @@
 
 - Webpack config for multi environments
 
+
+## Update packages
+```
+    npx npm-check-updates -u
+```
+
 ## Folder Structure
 ```
 assets/
 public/
 src/
 
-babel.config.js
+.babelrc
 package.json
 webpack.config.js
 ```
 
 ## Run project
 ```
-    "start": "webpack-dev-server --open --port 3721",
-    "build": "webpack"
+    "start": "webpack serve --env development",
+    "build": "webpack --env development"
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,46 @@
-const path = require('path');
+const path = require("path");
 // create html refering the bundle.js
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-    entry: './src/index.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/index.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/, use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.scss$/, use: ['style-loader', "css-loader", "sass-loader"]
-            },
-        ]
-    },
-    mode: 'development',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-            favicon: 'public/favicon.ico' 
-        })
+  entry: "./src/index-redux-breakdown",
+  //   entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 3721,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      favicon: "public/favicon.ico",
+    }),
+  ],
 };
 
-module.exports = config;   
+module.exports = config;
