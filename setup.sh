2b2dfc445a8829c486ee6fce9a25479b95259314
benjaminhuanghuang <bhuang@rms.com>
2020-12-10 21:41:58 -0800

    updaste

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ npm i webpack-cli webpack webpack-dev-server html-webpack-plugin -D
 
 npm i react react-dom prop-types
 
-npm i redux react-redux reselect
+npm i redux react-redux redux-thunk reselect
 npm i react-router-dom
 
 npm i babel-loader @babel/core @babel/preset-env @babel/preset-react -D
